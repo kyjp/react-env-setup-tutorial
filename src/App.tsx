387aad1305cd0ba6f10f85b908efda4a6367b217
yoshihara <yoshihara@ekzm.co.jp>
2024-04-30 15:52:36 +0900

    mod husky

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import TextInput from './components/Blog/input/TextInput'
+
+function App() {
+
+  return (
+    <>
+      <TextInput />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Blog/input/TextInput.test.tsx
@@ -0,0 +1,17 @@
+import {render, screen} from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+import TextInput from "./TextInput"
+
+test("TextInput Component test", async () => {
+    const user = userEvent.setup()
+    render(<TextInput />)
+    const inputElement = screen.getByRole("textbox")
+    expect(inputElement).toBeInTheDocument()
+    await user.type(inputElement, 'Hello World')
+    expect(screen.getByText('Hello World')).toBeInTheDocument()
+})
+
+test('TextInput Event Test', () => {
+    await userEvent.type(inputElement, 'Hello World')
+    expect(screen.getByText('Hello World')).toBeInTheDocument()
+})
